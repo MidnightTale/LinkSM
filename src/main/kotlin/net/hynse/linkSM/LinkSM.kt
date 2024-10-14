94996effac10678c38c41f3c329fd4b1594af5e6
MidnightTale <midnighttale24@gmail.com>
2024-10-14 13:27:30 +0700

    Initial commit

---
+++ src/main/kotlin/net/hynse/linkSM/LinkSM.kt
@@ -0,0 +1,14 @@
+package net.hynse.linkSM
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LinkSM : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
